2daff27dd8fd9584d2afe2af486144d7019d5ca9
Spindlyskit <josh.lavelle1@gmail.com>
2018-10-30 12:43:01 +0000

    FIX a bug where too many commas were expected in arguments

--- src/extensions/message.js
+++ src/extensions/message.js
@@ -500,7 +500,7 @@ module.exports = Structures.extend('Message', Message => {
 		static parseArgs(argString, argCount, allowSingleQuote = true) {
 			/* eslint-disable max-len */
 			const commaRegex = allowSingleQuote ? /[\s]*(?:(["'])([^]*?)\1|([^\s,]+))\s*[,]*/g : /[\s]*(?:(")([^]*?)\1|([^\s,]+))\s*[,]*/g;
-			const re = argString.split(',').length > argCount ? commaRegex : (allowSingleQuote ? /\s*(?:("|')([^]*?)\1|(\S+))\s*/g : /\s*(?:(")([^]*?)"|(\S+))\s*/g);
+			const re = argString.split(',').length >= argCount - 1 ? commaRegex : allowSingleQuote ? /\s*(?:("|')([^]*?)\1|(\S+))\s*/g : /\s*(?:(")([^]*?)"|(\S+))\s*/g;
 			/* eslint-enable max-len */
 			const result = [];
 			let match = [];
